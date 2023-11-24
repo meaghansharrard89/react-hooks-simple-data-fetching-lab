38d6e51b88df0eb2b68d3dbb0f14335367772612
Meaghan <meaghan.sharrard89@gmail.com>
2023-11-24 10:42:40 -0800

    passing tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+
+    const [randomImage, setRandomImage] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then((data) => {
+        //name of image in the API ("message")
+            setRandomImage(data.message);
+        })
+    });
+
+
+if (!randomImage) {
+    return <p>"Loading..."</p>
+}
+
+return (
+
+<img src={randomImage} alt="A Random Dog"/>
+)
+
+}
+
+export default App
